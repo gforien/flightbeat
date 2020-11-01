d7f99ed6a9597906f911459e330314170b83885a
Gabriel Forien <gforien+dev@gmail.com>
2020-11-01 23:28:53 +0100

    added beat :sparkles:

---
+++ flightbeat/README.md
@@ -0,0 +1,174 @@
+# flightbeat :airplane::zap:
+
+![](https://img.shields.io/badge/5TC-ELK-blueviolet)
+![](https://img.shields.io/badge/license-Apache-grey)
+
+#### Beat written in Go to retrieve flight prices from Skyscanner API and insert them into an elasticsearch cluster
+
+=> Explanation diagram <=
+
+<!--- Here are technologies used
+<p style = text-align:center;>
+    <img  src="https://upload.wikimedia.org/wikipedia/fr/3/38/Guitar_Hero_Logo.png" alt="Guitar Hero" height="145" width="199">
+    <img src="https://www.neonmag.fr/content/uploads/2019/04/color-spotify-logo.jpg" alt="Spotify" height="145" width="214">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png" alt="Osu" height="145" width="145">
+</p>
+Or more simply, a GIF of the app functionning
+![](screenshot.gif)
+--->
+
+## :construction_worker: Build and run the beat
+```bash
+$ mage build
+$ ./flightbeat -e -d "*"
+# input : flightbeat.yml (default)
+# output: console (default)
+```
+
+## :rocket: Deploy
+The beat has to always be active on 1 node (out of 16).
+A periodic NodeJS job, **leader.js** makes sure that there is always a leader designated to run the beat.
+*(and because a NodeJS job doesn't handle hard reboot, a cron job makes sure every hour that **leader.js** is up and running).*
+In case of failure of the leader, the nodes perform a leader election to elect a new leader who will run the beat.
+
+
+## :wrench: Generate your Beat from elastic/beats with mage (oct. 2020)
+1. install go
+2. `go get github.com/elastic/beats`
+3. `go get github.com/magefile/mage`
+4. `cd elastic/beats; mage GenerateCustomBeat` (fill everything explicitly, there is no default value)
+5. `cd gforien/custombeat`
+6. disable W10 App execution alias  + copy python.exe to go/bin/python3.exe  + disable W10 real-time protection (re-enable after)
+7. `make setup; make`
+
+
+## :books: Sources
+This project is sampled from :
+- [Beats developer guide](https://www.elastic.co/guide/en/beats/devguide/current/new-beat.html)
+- [Go by example](https://gobyexample.com/)
+- [HTTP requests in Go](https://medium.com/rungo/making-external-http-requests-in-go-eb4c015f8839)
+- [Biggest airports in the world](https://gettocenter.com/airports/top-100-airports-in-world/1000)
+- [Flight routes database](https://www.kaggle.com/open-flights/flight-route-database)
+##
+#### [Gabriel FORIEN](https://github.com/gforien)
+![](https://upload.wikimedia.org/wikipedia/commons/b/b9/Logo_INSA_Lyon_%282014%29.svg)
+
+
+
+<!-- # {Beat}
+
+Welcome to {Beat}.
+
+Ensure that this folder is at the following location:
+`${GOPATH}/src/github.com/gforien/flightbeat`
+
+## Getting Started with {Beat}
+
+### Requirements
+
+* [Golang](https://golang.org/dl/) 1.7
+
+### Init Project
+To get running with {Beat} and also install the
+dependencies, run the following command:
+
+```
+make setup
+```
+
+It will create a clean git history for each major step. Note that you can always rewrite the history if you wish before pushing your changes.
+
+To push {Beat} in the git repository, run the following commands:
+
+```
+git remote set-url origin https://github.com/gforien/flightbeat
+git push origin master
+```
+
+For further development, check out the [beat developer guide](https://www.elastic.co/guide/en/beats/libbeat/current/new-beat.html).
+
+### Build
+
+To build the binary for {Beat} run the command below. This will generate a binary
+in the same directory with the name flightbeat.
+
+```
+make
+```
+
+
+### Run
+
+To run {Beat} with debugging output enabled, run:
+
+```
+./flightbeat -c flightbeat.yml -e -d "*"
+```
+
+
+### Test
+
+To test {Beat}, run the following command:
+
+```
+make testsuite
+```
+
+alternatively:
+```
+make unit-tests
+make system-tests
+make integration-tests
+make coverage-report
+```
+
+The test coverage is reported in the folder `./build/coverage/`
+
+### Update
+
+Each beat has a template for the mapping in elasticsearch and a documentation for the fields
+which is automatically generated based on `fields.yml` by running the following command.
+
+```
+make update
+```
+
+
+### Cleanup
+
+To clean  {Beat} source code, run the following command:
+
+```
+make fmt
+```
+
+To clean up the build directory and generated artifacts, run:
+
+```
+make clean
+```
+
+
+### Clone
+
+To clone {Beat} from the git repository, run the following commands:
+
+```
+mkdir -p ${GOPATH}/src/github.com/gforien/flightbeat
+git clone https://github.com/gforien/flightbeat ${GOPATH}/src/github.com/gforien/flightbeat
+```
+
+
+For further development, check out the [beat developer guide](https://www.elastic.co/guide/en/beats/libbeat/current/new-beat.html).
+
+
+## Packaging
+
+The beat frameworks provides tools to crosscompile and package your beat for different platforms. This requires [docker](https://www.docker.com/) and vendoring as described above. To build packages of your beat, run the following command:
+
+```
+make release
+```
+
+This will fetch and create all images required for the build process. The whole process to finish can take several minutes.
+ -->
---
+++ flightbeat/beater/flightbeat.go
@@ -0,0 +1,230 @@
+package beater
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"errors"
+    "net/http"
+    "encoding/json"
+    "io/ioutil"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/gforien/flightbeat/config"
+)
+
+type flightbeat struct {
+	done   chan struct{}
+	config config.Config
+	client beat.Client
+}
+
+type skRoute struct {
+	Orig struct {
+		Rank       int    `json:"rank"`
+		Name       string `json:"name"`
+		Iata       string `json:"iata"`
+		City       string `json:"city"`
+		Country    string `json:"country"`
+		Passengers int    `json:"passengers"`
+		Geo        string `json:"geo"`
+	} `json:"orig"`
+	Dest struct {
+		Rank       int    `json:"rank"`
+		Name       string `json:"name"`
+		Iata       string `json:"iata"`
+		City       string `json:"city"`
+		Country    string `json:"country"`
+		Passengers int    `json:"passengers"`
+		Geo        string `json:"geo"`
+	} `json:"dest"`
+	Carrier string
+	Direct bool
+	MinPrice int
+}
+
+type skResponse struct {
+	Quotes []struct {
+		QuoteID     int  `json:"QuoteId"`
+		MinPrice    int  `json:"MinPrice"`
+		Direct      bool `json:"Direct"`
+		OutboundLeg struct {
+			OriginID      int    `json:"OriginId"`
+			DestinationID int    `json:"DestinationId"`
+			DepartureDate string `json:"DepartureDate"`
+		} `json:"OutboundLeg"`
+		QuoteDateTime string `json:"QuoteDateTime"`
+	} `json:"Quotes"`
+	Carriers []struct {
+		CarrierID int    `json:"CarrierId"`
+		Name      string `json:"Name"`
+	} `json:"Carriers"`
+	Places []struct {
+		Name           string `json:"Name"`
+		Type           string `json:"Type"`
+		PlaceID        int    `json:"PlaceId"`
+		IataCode       string `json:"IataCode"`
+		SkyscannerCode string `json:"SkyscannerCode"`
+		CityName       string `json:"CityName"`
+		CityID         string `json:"CityId"`
+		CountryName    string `json:"CountryName"`
+	} `json:"Places"`
+	Currencies []struct {
+		Code                        string `json:"Code"`
+		Symbol                      string `json:"Symbol"`
+		ThousandsSeparator          string `json:"ThousandsSeparator"`
+		DecimalSeparator            string `json:"DecimalSeparator"`
+		SymbolOnLeft                bool   `json:"SymbolOnLeft"`
+		SpaceBetweenAmountAndSymbol bool   `json:"SpaceBetweenAmountAndSymbol"`
+		RoundingCoefficient         int    `json:"RoundingCoefficient"`
+		DecimalDigits               int    `json:"DecimalDigits"`
+	} `json:"Currencies"`
+}
+
+func (r skRoute) toString() string {
+    bytes, err := json.Marshal(r)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+    return string(bytes)
+}
+
+func getRoutes() []skRoute {
+    // routes := make([]skRoute, 42000)
+    routes := make([]skRoute, 41748)
+
+	// read JSON file
+    raw, err := ioutil.ReadFile("./skyscanner_routes.json")
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    err = json.Unmarshal(raw, &routes)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+    return routes
+}
+
+// Run starts flightbeat.
+// logp.Info("flightbeat is running! Hit CTRL-C to stop it.")
+func (bt *flightbeat) Run(b *beat.Beat) error {
+
+
+	var err error
+	bt.client, err = b.Publisher.Connect()
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(bt.config.Period)
+	routes := getRoutes()
+	counter := 0
+
+
+	for {
+		fmt.Println("Query "+routes[counter].Orig.Iata+" => "+routes[counter].Dest.Iata)
+
+		select {
+		case <-bt.done:
+			return nil
+		case <-ticker.C:
+		}
+
+		quote, err := request(routes, counter)
+	    if err != nil {
+	        fmt.Println(err.Error())
+	        routes = remove(routes, counter)
+			fmt.Println("len(routes) = ", len(routes))
+
+	    } else {
+			event := beat.Event{
+				Timestamp: time.Now(),
+				Fields: common.MapStr{
+					"Orig": quote.Orig,
+					"Dest": quote.Dest,
+					"Carrier": quote.Carrier,
+					"Direct": quote.Direct,
+					"MinPrice": quote.MinPrice,
+				}}
+
+			bt.client.Publish(event)
+			logp.Info("Event sent")
+			counter = (counter+1)%41748
+	    }
+	}
+
+	return nil
+}
+
+func remove(s []skRoute, i int) []skRoute {
+    s[i] = s[len(s)-1]
+    return s[:len(s)-1]
+}
+
+// Stop stops flightbeat.
+func (bt *flightbeat) Stop() {
+	bt.client.Close()
+	close(bt.done)
+}
+
+func request(routes []skRoute, index int) (skRoute, error) {
+
+	origin := routes[index].Orig.Iata
+	dest := routes[index].Dest.Iata
+	now:= time.Now()
+	outboundpartialdate := fmt.Sprintf("%d-%d-%[2]d", now.Year(), now.Month(), now.Day())
+
+	url := fmt.Sprintf("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/%s/%s/%s", origin, dest, outboundpartialdate)
+	req, err := http.NewRequest("GET", url, nil)
+	if (err != nil) { fmt.Println(err.Error()) }
+
+	req.Header.Add("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", os.Getenv("FLIGHT_API_KEY"))
+
+	res, err := http.DefaultClient.Do(req)
+	if (err != nil) { fmt.Println(err.Error()) }
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if (err != nil) { fmt.Println(err.Error()) }
+
+	data := skResponse{}
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+	if len(data.Quotes) > 0 {
+
+		result := skRoute{
+			Orig: routes[index].Orig,
+			Dest: routes[index].Dest,
+			Carrier: data.Carriers[0].Name,
+			Direct: data.Quotes[0].Direct,
+			MinPrice: data.Quotes[0].MinPrice}
+		return result, nil
+	} else {
+		return skRoute{}, errors.New("Returned empty")
+	}
+}
+
+
+// New creates an instance of flightbeat.
+func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
+	c := config.DefaultConfig
+	if err := cfg.Unpack(&c); err != nil {
+		return nil, fmt.Errorf("Error reading config file: %v", err)
+	}
+
+	bt := &flightbeat{
+		done:   make(chan struct{}),
+		config: c,
+	}
+	return bt, nil
+}

18f9bb9d77b7566ebaae6a7011d08f737c42751e
Gabriel Forien <gforien+dev@gmail.com>
2020-11-02 16:38:01 +0100

    fixed deployment script :rocket:

--- leader-election/Cluster.ps1
+++ leader-election/Cluster.ps1
@@ -22,8 +22,8 @@ switch($argsString) {
     "remove crons"          { &{ 1..16 | %{ echo tc$_; ssh tc$_ "crontab -r"}}}
 
     # active jobs
-    "list jobs"             { &{ 1..16 | %{ ssh tc$_ "ps -eo 'pid,cmd' | grep -E '^.{5} node'" }}}
-    "kill jobs"             { &{ echo "Getting jobs..."; 1..16 | %{ ssh tc$_ "ps -eo 'pid,cmd' | grep -E '^.{5} node' |  grep -oE '^.{5}'" } > jobs.log; echo "Found jobs"; cat jobs.log; echo "Killing jobs..."; 1..16 | %{ ssh tc$_ "kill $((cat .\jobs.log) -join `" `") 2>/dev/null"; echo "$_ => $?" } ; rm  jobs.log }}
+    "list jobs"             { &{ 1..16 | %{ ssh tc$_ "ps -eo 'pid,cmd' | grep -E '^.{5} node leader.js'" }}}
+    "kill jobs"             { &{ echo "Getting jobs..."; 1..16 | %{ ssh tc$_ "ps -eo 'pid,cmd' | grep -E '^.{5} node leader.js' |  grep -oE '^.{5}'" } > jobs.log; echo "Found jobs"; cat jobs.log; echo "Killing jobs..."; 1..16 | %{ ssh tc$_ "kill $((cat .\jobs.log) -join `" `") 2>/dev/null"; echo "$_ => $?" } ; rm  jobs.log }}
     "start jobs"            { &{ 1..16 | %{ ssh tc$_ "cd ~/flightbeat/leader-election; node leader.js $_ >> ~/electionlog_$_ 2>&1 &"; echo "tc$_ => $?" }}}
 
     # log files
